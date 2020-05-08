e416a0a82620490ede1b146c99a52668bc835f6c
gea-ecobricks <64298789+gea-ecobricks@users.noreply.github.com>
2020-05-08 20:34:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,134 +1,62 @@
-// A reference to Stripe.js
-var stripe;
-
-var orderData = {
-  items: [{ id: "photo-subscription" }],
-  currency: "usd"
+// Create a Checkout Session with the selected plan ID
+var createCheckoutSession = function(planId) {
+  return fetch("/create-checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      planId: planId
+    })
+  }).then(function(result) {
+    return result.json();
+  });
 };
 
-// Disable the button until we have Stripe set up on the page
-document.querySelector("button").disabled = false;
+// Handle any errors returned from Checkout
+var handleResult = function(result) {
+  if (result.error) {
+    var displayError = document.getElementById("error-message");
+    displayError.textContent = result.error.message;
+  }
+};
 
-fetch("/create-payment-intent", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(orderData)
-})
+/* Get your Stripe publishable key to initialize Stripe.js */
+fetch("/setup")
   .then(function(result) {
     return result.json();
   })
-  .then(function(data) {
-    return setupElements(data);
-  })
-  .then(function({ stripe, card, clientSecret }) {
-    document.querySelector("button").disabled = false;
+  .then(function(json) {
+    var publicKey = json.publicKey;
+    var basicPlanId = json.basicPlan;
+    var proPlanId = json.proPlan;
 
-    // Handle form submission.
-    var form = document.getElementById("payment-form");
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      // Initiate payment when the submit button is clicked
-      pay(stripe, card, clientSecret);
-    });
+    var stripe = Stripe(publicKey);
+    // Setup event handler to create a Checkout Session when button is clicked
+    document
+      .getElementById("basic-plan-btn")
+      .addEventListener("click", function(evt) {
+        createCheckoutSession(basicPlanId).then(function(data) {
+          // Call Stripe.js method to redirect to the new Checkout page
+          stripe
+            .redirectToCheckout({
+              sessionId: data.sessionId
+            })
+            .then(handleResult);
+        });
+      });
+
+    // Setup event handler to create a Checkout Session when button is clicked
+    document
+      .getElementById("pro-plan-btn")
+      .addEventListener("click", function(evt) {
+        createCheckoutSession(proPlanId).then(function(data) {
+          // Call Stripe.js method to redirect to the new Checkout page
+          stripe
+            .redirectToCheckout({
+              sessionId: data.sessionId
+            })
+            .then(handleResult);
+        });
+      });
   });
-
-// Set up Stripe.js and Elements to use in checkout form
-var setupElements = function(data) {
-  stripe = Stripe(data.publishableKey);
-  var elements = stripe.elements();
-  var style = {
-    base: {
-      color: "#32325d",
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSmoothing: "antialiased",
-      fontSize: "16px",
-      "::placeholder": {
-        color: "#aab7c4"
-      }
-    },
-    invalid: {
-      color: "#fa755a",
-      iconColor: "#fa755a"
-    }
-  };
-
-  var card = elements.create("card", { style: style });
-  card.mount("#card-element");
-
-  return {
-    stripe: stripe,
-    card: card,
-    clientSecret: data.clientSecret
-  };
-};
-
-/*
- * Calls stripe.confirmCardPayment which creates a pop-up modal to
- * prompt the user to enter extra authentication details without leaving your page
- */
-var pay = function(stripe, card, clientSecret) {
-  changeLoadingState(true);
-
-  // Initiate the payment.
-  // If authentication is required, confirmCardPayment will automatically display a modal
-  stripe
-    .confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: card
-      }
-    })
-    .then(function(result) {
-      if (result.error) {
-        // Show error to your customer
-        showError(result.error.message);
-      } else {
-        // The payment has been processed!
-        orderComplete(clientSecret);
-      }
-    });
-};
-
-/* ------- Post-payment helpers ------- */
-
-/* Shows a success / error message when the payment is complete */
-var orderComplete = function(clientSecret) {
-  // Just for the purpose of the sample, show the PaymentIntent response object
-  stripe.retrievePaymentIntent(clientSecret).then(function(result) {
-    var paymentIntent = result.paymentIntent;
-    var paymentIntentJson = JSON.stringify(paymentIntent, null, 2);
-
-    document.querySelector(".sr-payment-form").classList.add("hidden");
-    document.querySelector("pre").textContent = paymentIntentJson;
-
-    document.querySelector(".sr-result").classList.remove("hidden");
-    setTimeout(function() {
-      document.querySelector(".sr-result").classList.add("expand");
-    }, 200);
-
-    changeLoadingState(false);
-  });
-};
-
-var showError = function(errorMsgText) {
-  changeLoadingState(false);
-  var errorMsg = document.querySelector(".sr-field-error");
-  errorMsg.textContent = errorMsgText;
-  setTimeout(function() {
-    errorMsg.textContent = "";
-  }, 4000);
-};
-
-// Show a spinner on payment submission
-var changeLoadingState = function(isLoading) {
-  if (isLoading) {
-    document.querySelector("button").disabled = true;
-    document.querySelector("#spinner").classList.remove("hidden");
-    document.querySelector("#button-text").classList.add("hidden");
-  } else {
-    document.querySelector("button").disabled = false;
-    document.querySelector("#spinner").classList.add("hidden");
-    document.querySelector("#button-text").classList.remove("hidden");
-  }
-};
