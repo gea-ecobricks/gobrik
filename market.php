11895f7ceca64a9e31c3bebb115b88f33be92d6d
Russell <russmaier@gmail.com>
2022-04-17 15:57:37 +0800

    upload

---
+++ market.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php $dist='49';?>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+
+<!-- Meta tags for page display and search engine listing
+AND UNIQUE to HTML Pages-->
+
+<title>GoBrik | Brik Market</title>
+<meta name="keywords" content="drop off ecobricks, exchange, trade, buy, gobrik, ecobrick app, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration,  plastic solved, drop off, exchange, marketplace">
+<meta name="description" content="The Brik Market is the place to trade, buy and sell ecobricks with ecobrickers and community hubs around the world.">
+
+
+<!-- Facebook Open Graph Tags for social sharing-->
+
+<meta property="og:url"           content="https://www.gobrik.com/market.php">
+<meta property="og:type"          content="website">
+<meta property="og:title"         content="GoBrik | Brik Market">
+<meta property="og:description"   content="The Brik Market is the place to trade, buy and sell ecobricks with ecobrickers and community hubs around the world." >
+<meta property="og:image"         content="https://www.gobrik.com/images/market-1000px.png" >
+<meta property="fb:app_id"  content="1781710898523821" >
+<meta property="og:image:width" content="1000" />
+<meta property="og:image:height" content="1028" />
+<meta property="og:image:alt"     content="An albatross exchange a brikcoin for an ecobrick"/>
+<meta property="og:locale" content="en_GB, id_ID, es_ES" />
+
+<!-- sets page meta tags:  be sure to add page name-->
+<?php require_once ("header.php");?>
+
+<!-- sets header and core of page:  be sure to add page name-->
+<?php require_once ("core.php");?>
+
+<!-- sets footer of page:  be sure to add page name-->
+<?php require_once ("footer.php");?>
+
+	 Testing the php: <?php echo ($lang); ;?>/<?php echo ($name); ;?>
+</body>
+</html>
