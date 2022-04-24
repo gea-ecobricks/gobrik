3f3a46a3476efb829e1d5456b40247dc1cb37b7c
Russell <russmaier@gmail.com>
2022-04-24 15:56:30 +0800

    update

--- disclosure.php
+++ disclosure.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<?php $dist='43';?>
+<?php $dist='42';?>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" >
 
 
@@ -13,22 +13,22 @@
 <!-- Meta tags for page display and search engine listing
 AND UNIQUE to HTML Pages-->
 
-<title>Regen Store | GoBrik.com</title>
-<meta name="keywords" content="green products, regenerative products, earth products, earthwands, earthcups, brikcoin, gobrik, regen store, green store">
-<meta name="description" content="Shop regenerative products directly from makers.">
+<title>2021 Plastic Transition Report | GoBrik.com</title>
+<meta name="keywords" content="plastic transition, disclosure, impact report, regen report, regenerativity report, ecological impacts">
+<meta name="description" content="The public disclosure of our enterprise's previous year plastic impacts and plastic transition.">
 
 
 <!-- Facebook Open Graph Tags for social sharing-->
 
 <meta property="og:url"           content="https://www.gobrik.com/store.php">
 <meta property="og:type"          content="website">
-<meta property="og:title"         content="Regen Store | GoBrik.com">
-<meta property="og:description"   content="Shop regenerative products directly from makers." >
-<meta property="og:image"         content="https://www.gobrik.com/images/regen-store-banner-flat-1000px.png" >
+<meta property="og:title"         content="2021 Plastic Transition Report | GoBrik.com">
+<meta property="og:description"   content="The public disclosure of our enterprise's previous year plastic impacts and plastic transition." >
+<meta property="og:image"         content="https://www.gobrik.com/images/disclosebanner.png" >
 <meta property="fb:app_id"  content="1781710898523821" >
 <meta property="og:image:width" content="1000" />
 <meta property="og:image:height" content="" />
-<meta property="og:image:alt"     content="Regeneartive green products"/>
+<meta property="og:image:alt"     content="Regeneartive reporting"/>
 <meta property="og:locale" content="en_GB, id_ID, es_ES" />
 
 
