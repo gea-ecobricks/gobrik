0624891c937223c0cce97cdf1a98686834741474
russmaier <russmaier@gmail.com>
2022-05-17 12:15:28 +0800

    cata

---
+++ catalyst.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php $dist='51';?>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+
+
+
+<!-- Meta tags for page display and search engine listing
+AND UNIQUE to HTML Pages-->
+<title>Catalyze | GoBrik.com</title>
+
+<meta name="keywords" content="company plastic tracking, ecological accounting, green accounting, regenerative reporting, carbon accounting, biodiversity tracking, disclosure, track, carbon, plastic, tracking, disclose, ecobricks, eco-bricks, eco bricks, eco briks, briks, plastic, plastic management, carbon sequestration, earthen ethics, plastic, recycling alternatives, incineration, plastic offsets, plastic solutions, upcycling, plastic sequestration, recycling alternative, incineration alternative, preventing plastic pollution, plastic pollution, regenerative plastic solution, waste, waste management, plastic waste">
+
+<meta name="description" content="A platform for keen green enterprises to track, disclose & accelerate their plastic transition.">
+
+
+<!-- Facebook Open Graph Tags for social sharing-->
+
+<meta property="og:url"           content="https://www.gobrik.com/catalyst">
+<meta property="og:type"          content="website">
+<meta property="og:title"         content="GoBrik Catalyst Program">
+<meta property="og:description"   content="A platform for keen green enterprises to track, disclose and accelearte their plastic transition." >
+<meta property="og:image"         content="pngs/catalyst-fb-banner.png"/>
+<meta property="fb:app_id"  content="1781710898523821" >
+<meta property="og:image:width" content="1000px" />
+<meta property="og:image:height" content="1059px" />
+<meta property="og:image:alt"     content="Catalyst Program Logo by the Global Ecobrick Alliance."/>
+<meta property="og:locale" content="en_GB, id_ID, es_ES" />
+
+<link rel="preload" as="image" href="https://ecobricks.org/pngs/catalyst-logo-500px.png">
+
+
+<!-- sets page meta tags:  be sure to add page name-->
+<?php require_once ("header.php");?>
+
+<!-- sets header and core of page:  be sure to add page name-->
+<?php require_once ("core.php");?>
+
+<!-- sets footer of page:  be sure to add page name-->
+<?php require_once ("footer.php");?>
+
+<script>
+(function() {
+      var linksOnPage = document.querySelectorAll("a");
+      var link = "";
+      for (var i = 0; i < linksOnPage.length; i++) {
+        link = linksOnPage[i];
+        link.setAttribute("onclick", "window.scrollTo(0, 0)");
+  
+      }
+    })();
+    </script>
+
+    
+</body>
+</html>
