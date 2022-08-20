02a373252c41faa7a0df34b742a5ac114779532c
russmaier <russmaier@gmail.com>
2022-08-20 19:05:32 +0800

    f

--- landing-code.php
+++ landing-code.php
@@ -1,6 +1,8 @@
     <div id="landing-content">
     
-    <br><div class="top-graphic"><img src="https://gobrik.com/svgs/Happy-turtle-dolphin-opti2.svg" width="450px"></div>
+    <!--<br><div class="top-graphic"><img src="https://gobrik.com/svgs/Happy-turtle-dolphin-opti2.svg" width="450px"></div>-->
+
+    <br><div class="top-graphic"><img src="https://gobrik.com/svgs/earth-home-animated-v3.svg" width="450px"></div>
 
         <div class="big-header">Let's Keep our Plastic out of the Biosphere!</div>
 
@@ -29,11 +31,11 @@
 
             <!--<a href="https://www.gobrik.com/#home/auth/google" class="social-button" id="google-connect"  onclick="topFunction()"> <span>Login with Google</span></a>
 
-              
-            <br><br>/images/mangotreebluesky400px.png
+              <img src="https://ecobricks.org/webp/world-ocean-day-2022-600px.webp" style="width: 400px;"></div>
+            <br><br>
         </div>-->
       
-        <br><br><br><br><div class="tree-coins"><img src="https://ecobricks.org/webp/world-ocean-day-2022-600px.webp" style="width: 400px;"></div>
+        <br><br><br><br><div class="tree-coins"><img src="https://ecobricks.org//images/mangotreebluesky400px.png" style="width: 400px;"></div>
 
         <div class="tree-text">
             <h5>Together we're securing plastic out of the biosphere to make building blocks, brikcoins and green spaces.</h5>
