95248b2155bc4d42d76285ce703c13d7d9f85ba6
russmaier <russmaier@gmail.com>
2022-09-09 11:57:01 +0800

    go

--- footer.php
+++ footer.php
@@ -113,9 +113,6 @@
 					</div>
 						
 				</div>
-			
-					
-						
 							
 									<div id="wcb" class="carbonbadge wcb-d"></div>
 								<script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer></script>
