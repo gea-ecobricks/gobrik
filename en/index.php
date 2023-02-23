6355363b67aedb904dcf1120df901becd642df6d
Russell Maier <russ@earthen.io>
2023-02-23 20:44:38 +0800

    165

--- en/index.php
+++ en/index.php
@@ -569,14 +569,14 @@ function scrollFunction() {
 
 	<div id="header" class="top-menu">
 		<div class="gobrik-logo">
-            <a href="/go.php#home"><img src="../svgs/gobrik-logo-static.svg?v1.1" width="185" height=""></a>
+            <a href="/go.php#home"><img src="../svgs/gobrik-logo-static.svg?v1.1" width="165" height=""></a>
         </div>
 		
         <div class="button-list" style="display:flex;">
             <div id="lang-button">
                 <div class="topnav-lang" id="myTopnav-lang">
                     <div class="dropdown-lang">
-                        <button class="dropbtn-lang"><img src="../svgs/language-button.svg" height="30px"></button>
+                        <button class="dropbtn-lang"><img src="../svgs/language-button2.svg" height="30px"></button>
                         <div class="dropdown-content-language">
                             <a href='../en/'>🇬🇧&nbsp;ENG</a>
                             <a href='../id/'>🇮🇩&nbsp;IND</a>
