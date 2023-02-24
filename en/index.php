6bb2a5da6339bb0421683d45327f6d265506da03
Russell Maier <russ@earthen.io>
2023-02-24 21:33:56 +0800

    go richard

--- en/index.php
+++ en/index.php
@@ -957,12 +957,9 @@ margin-left: auto;
     left:0;
     margin-top: 22px;
     margin-left:20px;
-
     border: none; 
     margin-right:10px;
     cursor:pointer;
-    background: url('../svgs/grey-emblem-button3.svg?v=2') no-repeat;
-    background-size: contain;
     height:30px;
     width:30px;
   }
@@ -971,8 +968,6 @@ margin-left: auto;
     border: none; 
     margin-right:10px;
     cursor:pointer;
-    background: url('../svgs/grey-emblem-button-over.svg') no-repeat;
-    background-size: contain;
     height:30px;
     width:30px;
   }
@@ -1092,162 +1087,7 @@ margin-left: auto;
   top: 0px;
 }
 
-</style>
 
-
-<script>
-
-/* RIGHT SETTINGS OVERLAY */
-function openSettings() {
-  document.getElementById("right-settings-overlay").style.width = "100%";
-  document.body.style.overflowY = "hidden";
-  document.body.style.maxHeight = "101vh";
-
-  var modal = document.getElementById('right-settings-overlay');
-
-function modalShow () {
-   modal.setAttribute('tabindex', '0');
-   modal.focus();
-}
-
-function focusRestrict ( event ) {
-  document.addEventListener('focus', function( event ) {
-    if ( modalOpen && !modal.contains( event.target ) ) {
-      event.stopPropagation();
-      modal.focus();
-    }
-  }, true);
-}
-}
-
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeSettings() {
-  document.getElementById("right-settings-overlay").style.width = "0%";
-  document.body.style.overflowY = "unset";
-document.body.style.maxHeight = "unset";
-  //document.body.style.height = "unset";
-} 
-
-
-/* LEFT MAIN MENU OVERLAY 
-
-Triggers the right share link panel*/
-
-function openMenu() {
-  document.getElementById("left-menu-overlay").style.width = "100%";
-  document.body.style.overflowY = "hidden";
-  //document.body.style.maxHeight = "101vh";
-
-  var modal = document.getElementById('left-menu-overlay');
-
-function modalShow () {
-   modal.setAttribute('tabindex', '0');
-   modal.focus();
-}
-
-function focusRestrict ( event ) {
-  document.addEventListener('focus', function( event ) {
-    if ( modalOpen && !modal.contains( event.target ) ) {
-      event.stopPropagation();
-      modal.focus();
-    }
-  }, true);
-}
-}
-
-
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeMenu() {
-  document.getElementById("left-menu-overlay").style.width = "0%";
-  document.body.style.overflowY = "unset";
-  //document.body.style.maxHeight = "unset";
-
-
-
-/* Knack OVERLAY 
-
-Triggers the right share link panel*/
-
-function openKnack() {
-  document.getElementById("knack-overlay-curtain").style.height = "100%";
-  document.body.style.overflowY = "hidden";
-  //document.body.style.maxHeight = "101vh";
-
-  var modal = document.getElementById('knack-overlay-curtain');
-
-  function modalShow () {
-   modal.setAttribute('tabindex', '0');
-   modal.focus();
-}
-
-function focusRestrict ( event ) {
-  document.addEventListener('focus', function( event ) {
-    if ( modalOpen && !modal.contains( event.target ) ) {
-      event.stopPropagation();
-      modal.focus();
-    }
-  }, true);
-}
-}
-
-
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeKnack() {
-  document.getElementById("knack-overlay-curtain").style.height = "0%";
-  document.body.style.overflowY = "unset";
-  //document.body.style.maxHeight = "unset";
-
-} 
-
-
-
-function modalCloseCurtains ( e ) {
-  if ( !e.keyCode || e.keyCode === 27 ) {
-    
-  document.body.style.overflowY = "unset";
-  document.getElementById("right-close-button2").style.position = "absolute";
-  document.getElementById("right-share-overlay").style.width = "0%";
-
-  document.getElementById("left-settings-overlay").style.width = "0%";
-  document.getElementById("knack-overlay-curtain").style.height = "0%";
-
-  }
-}
-
-document.addEventListener('keydown', modalCloseCurtains);
-
-
-/*
-window.onscroll = function() {scrollFunction()};
-
-//Scroll on arrival 
-function scrollFunction() {
-
-    const mediaQuery = window.matchMedia('(max-width: 700px)')
-
-   //AFTER
-   if ((mediaQuery.matches)  || (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)) {
-    
-   
-    document.getElementById("header").style.height = "50px";
-    document.getElementById("lang-button").style.opacity = "1";
-    document.getElementById("header").style.position = "fixed";
-    
- 
-   } else {
-     //BEFORE
-     document.getElementById("header").style.height = "90vh";
-     document.getElementById("header").style.position = "relative";
-     
- 
-   }
- }
-
-*/
- </script>
 
 
  
