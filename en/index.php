ad6bca10581e6fcbf0324460e335a36e7ea45cb8
Russell Maier <russ@earthen.io>
2023-02-24 19:18:41 +0800

    settings-cr

--- en/index.php
+++ en/index.php
@@ -698,19 +698,6 @@ border-color: #00a112 !important;
 
 }
 
-#right-close-button img {
-height: 50px;
-padding-top: 40px;
-transition: 0.3s;
-
-}
-
-#right-close-button img:hover {
-  height: 70px;
-  padding-top:35px;
-
-
-}
 
 
 /* -------------------------------------------------------------------------- */
@@ -1190,52 +1177,13 @@ if (!$conn) {
            <!-- <h6>Learn some more about<br><a href="#ecobricks">Ecobricks</a>, <a href="#gea">the GEA</a>, <a href="#brikcoins">Brikcoins</a> and <a href="#aes">AES Plastic Offsets</a></h6>-->
         </div><!--closes Landing content-->
     </div>
-    <div id="right-settings-overlay" class="overlay-settings">
-
-<div id="right-close-button">
-        <button type="button" style="cursor:pointer" onclick="closeSettings()" aria-label="Click to close settings page" class="x-button"></button>
-    </div>
-
-<div class="overlay-content-settings">
-        
-            <div class="compro-toggle">
-            <dark-mode-toggle
-            id="dark-mode-toggle-1"
-            legend=""
-            appearance="switch"
-            dark="Dark"
-            light="Light&nbsp;&nbsp;&nbsp;"
-            remember="Remember this"
-            ></dark-mode-toggle>
-                        </div>
-                        
-        <div id="languages">
-        <a href="../en/index.html" aria-label="Switch to the English version"><div class="language-selector">EN</div></a>
-            <div class="language-selector" title="Désolé, pas encore traduit !" aria-label="French not yet active">FR</div>
-            <div class="language-selector" title="Maaf, belum diterjemahkan!" aria-label="Indonesian not active">IN</div>
-        </div> 
-
-     
-
-        <div id="text-adjust">
-            <button id="increase-plugin-ac" aria-label="Increase Text Size">+A</button>
-            <button id="normal-plugin-ac" aria-label="Return Text Size to Default">A</button>
-            <button id="decrease-plugin-ac" aria-label="Decrease Text Size">A-</button>
-        </div>
+   
+    <?php require_once ("settings-curtain.php");?>
 
 
-        <div class="settings-label"><div class="accessibility-plugin-ac">Contrast</div></div>
-        
-        <input aria-label="Contrast" type="range" id="contrast-range-scale" min="0" max="100" value="100" onchange="Contrast(this)">
-
-        <div class="settings-label"><div class="accessibility-plugin-ac">Sepia</div></div>
-        <input aria-label="Sepia" type="range" id="sepia-range-scale"  min="0" max="100" value="0" onchange="Sepia(this)">
-
-        <div class="settings-label"><div class="accessibility-plugin-ac">Brightness</div></div>
-        <input aria-label="Brightness" type="range" id="brightness-range-scale" min="0" max="100" value="100" onchange="Brightness(this)">
-        
-    </div>                                                              
-</div>
+<div id="containerSepia"></div>
+  <div id="containerContrast"></div>
+  <div id="containerBrightness"></div>
     
 
     <div style="z-index:5;">
@@ -1246,9 +1194,7 @@ if (!$conn) {
 
             </div>
 
-            <div id="containerSepia"></div>
-  <div id="containerContrast"></div>
-  <div id="containerBrightness"></div>
+        
   
 
 </div>
--- en/settings-curtain.php
+++ en/settings-curtain.php
@@ -1,46 +1,46 @@
- <div id="right-settings-overlay" class="overlay-settings">
+<div id="right-settings-overlay" class="overlay-settings">
 
-    <div id="right-close-button">
-            <span style="cursor:pointer" onclick="closeSettings()" aria-label="Click to close settings page"><img src="../svgs/left-x.svg" alt="Close settings button"></span>
+<div id="right-close-button">
+        <button type="button" style="cursor:pointer" onclick="closeSettings()" aria-label="Click to close settings page" class="x-button"></button>
+    </div>
+
+<div class="overlay-content-settings">
+        
+            <div class="compro-toggle">
+            <dark-mode-toggle
+            id="dark-mode-toggle-1"
+            legend=""
+            appearance="switch"
+            dark="Dark"
+            light="Light&nbsp;&nbsp;&nbsp;"
+            remember="Remember this"
+            ></dark-mode-toggle>
+                        </div>
+                        
+        <div id="languages">
+        <a href="../en/index.html" aria-label="Switch to the English version"><div class="language-selector">EN</div></a>
+            <div class="language-selector" title="Désolé, pas encore traduit !" aria-label="French not yet active">FR</div>
+            <div class="language-selector" title="Maaf, belum diterjemahkan!" aria-label="Indonesian not active">IN</div>
+        </div> 
+
+     
+
+        <div id="text-adjust">
+            <button id="increase-plugin-ac" aria-label="Increase Text Size">+A</button>
+            <button id="normal-plugin-ac" aria-label="Return Text Size to Default">A</button>
+            <button id="decrease-plugin-ac" aria-label="Decrease Text Size">A-</button>
         </div>
-    
-    <div class="overlay-content-settings">
-            
-                <div class="compro-toggle">
-                <dark-mode-toggle
-                id="dark-mode-toggle-1"
-                legend=""
-                appearance="switch"
-                dark="Dark"
-                light="Light&nbsp;&nbsp;&nbsp;"
-                remember="Remember this"
-                ></dark-mode-toggle>
-                            </div>
-                            
-            <div id="languages">
-            <a href="../en/index.html" aria-label="Switch to the English version"><div class="language-selector">EN</div></a>
-                <div class="language-selector" title="Désolé, pas encore traduit !" aria-label="French not yet active">FR</div>
-                <div class="language-selector" title="Maaf, belum diterjemahkan!" aria-label="Indonesian not active">IN</div>
-            </div> 
-
-         
-
-            <div id="text-adjust">
-                <button id="increase-plugin-ac" aria-label="Increase Text Size">+A</button>
-                <button id="normal-plugin-ac" aria-label="Return Text Size to Default">A</button>
-                <button id="decrease-plugin-ac" aria-label="Decrease Text Size">A-</button>
-            </div>
 
 
-            <div class="settings-label"><div class="accessibility-plugin-ac">Contrast</div></div>
-            
-            <input aria-label="Contrast" type="range" id="contrast-range-scale" min="0" max="100" value="100" onchange="Contrast(this)">
+        <div class="settings-label"><div class="accessibility-plugin-ac">Contrast</div></div>
+        
+        <input aria-label="Contrast" type="range" id="contrast-range-scale" min="0" max="100" value="100" onchange="Contrast(this)">
 
-            <div class="settings-label"><div class="accessibility-plugin-ac">Sepia</div></div>
-            <input aria-label="Sepia" type="range" id="sepia-range-scale"  min="0" max="100" value="0" onchange="Sepia(this)">
+        <div class="settings-label"><div class="accessibility-plugin-ac">Sepia</div></div>
+        <input aria-label="Sepia" type="range" id="sepia-range-scale"  min="0" max="100" value="0" onchange="Sepia(this)">
 
-            <div class="settings-label"><div class="accessibility-plugin-ac">Brightness</div></div>
-            <input aria-label="Brightness" type="range" id="brightness-range-scale" min="0" max="100" value="100" onchange="Brightness(this)">
-            
-        </div>                                                              
+        <div class="settings-label"><div class="accessibility-plugin-ac">Brightness</div></div>
+        <input aria-label="Brightness" type="range" id="brightness-range-scale" min="0" max="100" value="100" onchange="Brightness(this)">
+        
+    </div>                                                              
 </div>
