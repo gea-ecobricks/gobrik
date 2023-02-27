13b54e1cae54d8b328d29d1c9d6a5dfabaf7346f
Russell Maier <russ@earthen.io>
2023-02-27 10:28:03 +0800

    details2

--- en/details-ecobrick-page.php
+++ en/details-ecobrick-page.php
@@ -1,33 +1,92 @@
 
+<!DOCTYPE html>
+<?php $lang='en';?>
+
+<head lang="en">
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
 <!--PAGE LANGUAGE:  ENGLISH
 Special Ecobrick View Page: v.1.0.1-->
 
 <!--Content Page template: v.1.0.0-->
 
-<?php require_once ("lang.php");
-echo <<<_END
 
-<!DOCTYPE html>
-
-<meta charset="UTF-8"> 
-
-<!-- this grabs the language identifier for the page so that it can used in the meta and canonical url variables-->
-
-<html lang="$lang">
-
-_END;?>
-
-<HEAD>  
-
-<!--Image files to preload that are unique to this page-->
-
-<link rel="preload" as="image" href="https://ecobricks.org/logos/gea-horizontal.svg">
-<link rel="preload" as="image" href="https://ecobricks.org/webp/brikmarket-400px.webp?v1.3">
-<link rel="preload" as="image" href="https://ecobricks.org/svgs/brik-market-banner3.svg">
-
-<?php require_once ("details-ecobrick-en.php");?>
 
 <?php require_once ("header.php");?>
+
+<?php
+   
+$servername = "localhost";
+$username = "ecobricks_brikchain_viewer";
+$password = "desperate-like-the-Dawn";
+$dbname = "ecobricks_gobrik_msql_db";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+
+<!-- ENGLISH ECOBRICK DETAILS PAGE -->
+
+<?php
+
+include 'ecobricks_env.php';
+
+// Get the contents from the Ecobrick table as an ordered View, using the serial_no from the URL.
+$serialNo = $_GET['serial_no'];
+
+// Refered to  https://www.w3schools.com/php/php_mysql_select_where.asp1
+$sql = "SELECT * FROM tb_ecobricks WHERE serial_no = " . $serialNo;
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	
+    //  echo "<h1> Use Serial Number from URL => " . $serialNo ."</h1>"; Output data of each row 
+    while($array = $result->fetch_assoc()) {
+
+		echo '<title>Ecobrick '. $array["serial_no"] .' | '. $array["weight_g"] .'g of plastic sequestered by '. $array["owner"] .' in '. $array["location_full"] .'.</title>';
+
+        echo '<meta name="description" content="An authenticated ecobrick that was published and archived on the brikcoin manual blockchain on ' . $array["last_validation_ts"] .'">';
+
+        if ( isset($array["vision"]) && $array["vision"] != '' ) {
+            echo '<meta name="description" content="'. $array["vision"] .'">';
+    
+        }
+
+		echo '<meta name="keywords" content="ecobrick, serial '. $array["owner"] .', '. $array["owner"] .', brikchain, brikcoin, brik record, plastic sequestration, recycling, alternative, sequestration of plastic, plastic offsetting, aes plastic, carbon sequestration. '. $array["location_full"] .'">';
+
+        echo '<meta property="og:url"           content="https://ecobricks.org/en/details-ecobrick-page.php?serial_no='. $array["serial_no"] .'"/>' ;
+        echo '<meta property="og:title"         content="Ecobrick '. $array["serial_no"] .' | '. $array["weight_g"] .'g of plastic sequestered by '. $array["owner"] .' in '. $array["location_full"] .'.">';
+        echo '<meta property="og:description"   content="An authenticated ecobrick that was published and archived on the brikcoin manual blockchain on ' . $array["last_validation_ts"] .'"/>';
+        echo '<meta property="og:image"         content="'. $array["ecobrick_full_photo_url"] .'"/>';
+        echo '<meta property="og:image:alt"     content="The brikchain record of an authenticated ecobrick on the brikchain"/>';
+        echo '<meta property="og:locale" content="en_GB" />';
+        echo '<meta property="og:type"          content="website">';
+ 	   
+
+        echo '<meta property="og:type" content="article" />
+        <meta property="og:site_name" content="Ecobricks.org" />
+        <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
+        <meta property="article:modified_time" content="'. $array["last_validation_ts"] .'" />
+        <meta property="og:image:type" content="image/png" />
+        <meta name="author" content="Global Ecobrick Alliance" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:label1" content="Est. reading time" />
+        <meta name="twitter:data1" content="15 minutes" /> ';
+
+    }
+
+} else {
+    echo '<META NAME="robots" CONTENT="noindex">';
+    echo '<title>No Ecobrick Found | Ecobricks.org</title>';
+    echo '<meta name="description" content="No data found for this ecobrick serial number.  Most likely this is because the brikchain data is still in migration."> ';
+}
+$conn->close();
+
+?>
 
 <!-- Required script for something?? -->
 
@@ -301,20 +360,7 @@ margin-bottom: 31px;}
 
 </head>
 
-<?php
-   
-$servername = "localhost";
-$username = "ecobricks_brikchain_viewer";
-$password = "desperate-like-the-Dawn";
-$dbname = "ecobricks_gobrik_msql_db";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
 
-?>
 											  
 <BODY id="full-page">
 
@@ -496,9 +542,11 @@ echo '
 
 <BODY id="full-page">
 
+<!--MENU INSERT-->
+
 	  ';
 
-require_once ("menus/menu-$lang.php");
+
 
 
 
