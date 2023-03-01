6de32d1cdabe13baad9d6d7341f7ea8ac43e7d27
Russell Maier <russ@earthen.io>
2023-03-01 18:00:59 +0800

    styles

--- en/details-ecobrick-page.php
+++ en/details-ecobrick-page.php
@@ -1,14 +1,18 @@
 
 <!DOCTYPE html>
-
 <?php $lang='en';?>
 
 <head lang="en">
 
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+<!--PAGE LANGUAGE:  ENGLISH
+Special Ecobrick View Page: v.1.0.1-->
+
+<!--Content Page template: v.1.0.0-->
+
+
 
 <?php require_once ("header.php");?>
-
 
 <?php
    
@@ -27,6 +31,7 @@ if (!$conn) {
 
 <?php include '../ssp.class.php';?>
 
+<!-- ENGLISH ECOBRICK DETAILS PAGE -->
 
 <?php
 
@@ -83,306 +88,60 @@ $conn->close();
 
 ?>
 
+<!-- Required script for something?? -->
+
 
 <STYLE>
 
-
-.vision-quote {
-  font-family: 'Mulish', Arial, Helvetica, sans-serif;
-  color: #222222;
-  font-weight: 300;
-  text-shadow: 0 0 10px #fff;
-}
-@media screen and (max-width: 769px) {
-	.vision-quote {
-      font-size: 2.4em;
-      line-height: 1.2;
-      padding: 10px 6% 0px 6%;
-	  margin-bottom: -18px;
-  }
-}
-@media screen and (min-width: 769px) {
-	.vision-quote {
-      font-size: 4em;
-      line-height: 1.3;
-      padding: 15px;
-	  margin-top: -40px;
-	  margin-bottom: -20px;
-  }
-}
-
-/* Sidebar/right column */
-
-.row-details {  
-  display: flex;
-  flex-wrap: wrap;
-  box-sizing: border-box;
-}
-
-@media screen and (min-width: 700px) {
-.main-details {
-  flex: 60%;
-
-  padding: 0px;
-  box-sizing: border-box;
-}
-}
-
-@media screen and (max-width: 700px) {
-.main-details {
-  flex: 100%;
-  box-sizing: border-box;
-}
-}
-
-@media screen and (min-width: 700px) {
-	.side-details {
-	flex: 40%;
-	padding-left: 10px;
-	box-sizing: border-box;
-	margin-top: 30px;
-	background: #ECECEC;
-	padding: 10px;
-	height: fit-content;
-		}
-	}
-
-@media screen and (max-width: 700px) {
-	.side-details {
-	flex: 90%;
-	margin: 0px 5% 0px 5%;
-	box-sizing: border-box;
-	background: #ECECEC;
-	padding: 10px;
-	
-	
-	}
-	}
-
-#data-chunk {margin: 16px;}
-
-	
-@media screen and (max-width: 700px) { 
-	.splash-content-block {
-		text-align: left;
-		min-height: 73vh;
-		padding: 50px 5% 25px 5%;
-		margin-bottom: 0px;
-		z-index: 5;
-       
-		background: url(../svgs/brik-market-banner3.svg) bottom;
-        background-color: #2A91DA;
-		
-    	background-repeat: no-repeat;
-		background-size: cover;
-		margin-top: 45px;
-		width: 100%;
-		display: flex;
- 		box-sizing: border-box;
-		flex-direction: column;
-	position: relative;
-
-	}
-}
-
-@media screen and (min-width: 700px) { 
-	.splash-content-block {
-		text-align: left;
-		min-height: 70vh;
-		padding: 0px 7% 0px 7%;
-		z-index: 5;
-		position: relative;
-		
-        
-		background: url(https://ecobricks.org/svgs/brik-market-banner3.svg) bottom;
-        background-color: #2A91DA;
-		
-    	background-repeat: no-repeat;
-		margin: -3px 0 -20px 0;
-		display: flex;
- 		 flex-wrap: wrap;
- 		 box-sizing: border-box;
-		  flex-direction: row;
-		  width: 100%;
-		  background-size: cover;
-
-	position: relative;
-
-
-}
-}
-
-
-@media screen and (min-width: 700px) { 
-.splash-image {
-  z-index: 5;
-  position: relative;
-  text-align: center;
-  flex: 35%;
-	padding: 0px;
-	box-sizing: border-box;
-	margin: auto;
-}
-}
-
-
-@media screen and (min-width: 700px) { 
-.splash-box {
-  z-index: 5;
-  position: relative;
-  flex: 65%;
-	padding: 10px 30px 0px 0px;
-	box-sizing: border-box;
-	text-align: left;
-    margin: auto;
-}
-}
-
-
-
-@media screen and (max-width: 700px) { 
-.splash-image {
-  z-index: 5;
-  position: relative;
-  text-align: left;
-
-  width: 100%;
-	padding: 0px;
-	box-sizing: border-box;
-	margin: 32px 8% -25px 4%;
-}
-}
-
-
-
-@media screen and (max-width: 700px) { 
-.splash-box {
-	position: relative;
-    flex: 100%;
-	padding: 10px 10px 0px 0px;
-	box-sizing: border-box;
-	text-align: right;
-    margin: auto;
-}
-}
-
-
-
-.splash-heading { 
-    /*font-family: 'Mulish', Arial, Helvetica, sans-serif;*/
-    font-family: Arvo, serif;
-  color: white;
-  font-weight: 500;
-  text-shadow: 0 0 8px black;
-
-}
-
-@media screen and (max-width: 700px) {
-	.splash-heading {
-      font-size: 3.0em;
-      line-height: 1.1;
-      margin: 10px 0;
-  }
-}
-
-@media screen and (min-width: 700px) {
-	.splash-heading {
-      font-size: 4em;
-      line-height: 1.3;
-      margin: 40px 0 10px;
-  }
-}
-
-
-.splash-sub {
-  font-family: 'Mulish', Arial, Helvetica, sans-serif;
-  color: #fff;
-  /*text-shadow: 0px 0px 10px #fff;*/
-  margin: 15px 0;
-  text-shadow: 0 0 7px black;
-}
-
-@media screen and (max-width: 700px) {
-	.splash-sub {
-		font-size: 1.25em;
-		line-height: 1.25;
-		font-weight: 400;
-  }
-}
-@media screen and (min-width: 700px) {
-	.splash-sub {
-		font-size: 2.5em;
-		line-height: 1.3;
-		font-weight: 400;
-  }
-}
-
-
-#splash-bar {
-	margin-top: -50px;
-	width: 100%;
-	
-	height:100px;	
-	
-	position: relative;
-	z-index: 0;
-
-box-shadow: 0 8px 7px rgba(85, 84, 84, 0.4);
-    background-color: #2A91DA;
-
-	-webkit-transform: skewY(-3deg);
-  -moz-transform: skewY(-3deg);
-  -ms-transform: skewY(-3deg);
-  -o-transform: skewY(-3deg);
-  transform: skewY(-3deg);
-	margin-bottom: 40px;
-
-}
-
-
-
-#side-module-desktop-only {
-	
-	border-radius: 12px;
-	margin-bottom: 20px;
-	margin-top: 10px;
-	padding: 15px 15px 28px 15px;
-	border-width: 1px;
-	text-align: center;
-	background-color: #ECECEC;
-	text-align: center;
-	border-color: white;
-}
-
-@media screen and (max-width: 700px) {
-  #side-module-desktop-only {   
-    display: none;
-  }
-}
-
-#side-module-desktop-mobile {
-	
-	border-radius: 12px;
-	margin-bottom: 20px;
-	padding: 15px 15px 28px 15px;
-	border-width: 1px;
-	text-align: center;
-	background-color: #ECECEC;
-	text-align: center;
-	border-color: white;
-  
-  }
 
 
 </style>
 
 </head>
 
+
+											  
+
 <body id="full-page" class="accessibility-plugin-ac">
 
-<?php require_once ("header.php");?>
-							  								
+				
+
+	<div id="header" class="top-menu">
+
+
+        <div><button type="button" class="main-menu-button" onclick="openMenu()" aria-label="Login to Gobrik"></button></div>
+
+		<div><button type="button" class="gobrik-logo" onclick="closeKnack()" aria-label="Return to Home page"></button></div>
+        
+       
+		
+        <div class="button-list" style="display:flex;position:absolute;right:0;width:fit-content;margin-right:15px;flex-flow:row;margin-top:22px;"m>
+          
+            
+                <div id="top-menu-login-button"><button type="button" class="top-menu-login-button" onclick="openKnack()" >
+                    login
+                </button></div>
+
+                <div><button type="button" class="top-settings-button" onclick="openSettings()" aria-label="Click to open settings page"></button></div>
+
+               <!-- <div><button type="button" style="background: none;border: none;" class="settings-">
+                    <img src="../svgs/language-button2.svg" height="30px">
+                </button></div>-->
+                   
+		    </div>
+        </div>
+    </div> 
+
+
+
+							  
+											  
+
+
+ 
 <?php 
+
+
 
 // Get the contents from the Ecobrick table as an ordered View, using the serial_no from the URL.
 $serialNo = $_GET['serial_no'];
