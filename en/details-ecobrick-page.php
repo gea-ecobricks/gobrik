b0cf029d9238197270bc5fd6c0743d0b3ba9c5a0
Russell Maier <russ@earthen.io>
2023-03-01 18:28:38 +0800

    go

--- en/details-ecobrick-page.php
+++ en/details-ecobrick-page.php
@@ -5,14 +5,9 @@
 <head lang="en">
 
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" >
-<!--PAGE LANGUAGE:  ENGLISH
-Special Ecobrick View Page: v.1.0.1-->
-
-<!--Content Page template: v.1.0.0-->
-
-
 
 <?php require_once ("header.php");?>
+
 
 <?php
    
@@ -88,60 +83,15 @@ $conn->close();
 
 ?>
 
-<!-- Required script for something?? -->
-
-
-<STYLE>
-
-
-
-</style>
-
 </head>
 
 
-											  
-
 <body id="full-page" class="accessibility-plugin-ac">
 
-				
-
-	<div id="header" class="top-menu">
-
-
-        <div><button type="button" class="main-menu-button" onclick="openMenu()" aria-label="Login to Gobrik"></button></div>
-
-		<div><button type="button" class="gobrik-logo" onclick="closeKnack()" aria-label="Return to Home page"></button></div>
-        
-       
-		
-        <div class="button-list" style="display:flex;position:absolute;right:0;width:fit-content;margin-right:15px;flex-flow:row;margin-top:22px;"m>
-          
-            
-                <div id="top-menu-login-button"><button type="button" class="top-menu-login-button" onclick="openKnack()" >
-                    login
-                </button></div>
-
-                <div><button type="button" class="top-settings-button" onclick="openSettings()" aria-label="Click to open settings page"></button></div>
-
-               <!-- <div><button type="button" style="background: none;border: none;" class="settings-">
-                    <img src="../svgs/language-button2.svg" height="30px">
-                </button></div>-->
-                   
-		    </div>
-        </div>
-    </div> 
-
-
-
-							  
-											  
-
+<?php include 'top-menu';?>
 
  
 <?php 
-
-
 
 // Get the contents from the Ecobrick table as an ordered View, using the serial_no from the URL.
 $serialNo = $_GET['serial_no'];
