894f55c64f91bf9e5ebfc99695ff79963d158acf
Russell Maier <russ@earthen.io>
2023-03-01 17:40:15 +0800

    try 2

---
+++ en/top-menu.php
@@ -0,0 +1,28 @@
+
+											  
+
+<div id="header" class="top-menu">
+
+
+    <div><button type="button" class="main-menu-button" onclick="openMenu()" aria-label="Login to Gobrik"></button></div>
+
+    <div><button type="button" class="gobrik-logo" onclick="closeKnack()" aria-label="Return to Home page"></button></div>
+    
+   
+    
+    <div class="button-list" style="display:flex;position:absolute;right:0;width:fit-content;margin-right:15px;flex-flow:row;margin-top:22px;"m>
+      
+        
+            <div id="top-menu-login-button"><button type="button" class="top-menu-login-button" onclick="openKnack()" >
+                login
+            </button></div>
+
+            <div><button type="button" class="top-settings-button" onclick="openSettings()" aria-label="Click to open settings page"></button></div>
+
+           <!-- <div><button type="button" style="background: none;border: none;" class="settings-">
+                <img src="../svgs/language-button2.svg" height="30px">
+            </button></div>-->
+               
+        </div>
+    </div>
+</div> 
