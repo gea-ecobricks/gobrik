26cc0101078f6faaeb79455f0e65346c093b6d98
Russell Maier <russ@earthen.io>
2023-03-05 17:58:47 +0800

    go

--- richard-scripts.js
+++ richard-scripts.js
@@ -75,7 +75,7 @@ Triggers the right share link panel*/
 function openKnack() {
   document.getElementById("knack-overlay-curtain").style.height = "100%";
   document.getElementById("top-menu-login-button").style.display = "none";
-  document.getElementById("top-close-button").style.display = "none";
+  document.getElementById("top-close-buttoner").style.display = "contents";
   document.getElementById("landing-page").style.display = "none";
   //document.body.style.overflowY = "hidden";
   //document.body.style.maxHeight = "101vh";
