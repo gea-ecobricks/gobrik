f844fe9d151183655d2f34d4bd8e0303e714b013
Russell Maier <russ@earthen.io>
2023-03-07 15:51:31 +0800

    go go

--- en/knack-curtain.php
+++ en/knack-curtain.php
@@ -1,4 +1,37 @@
 <div id="knack-overlay-curtain" class="knack-overlay">
+
+
+<div id="header-knack" class="top-menu">
+
+
+    <div><button type="button" class="main-menu-button" onclick="openMenu()" aria-label="Login to Gobrik"></button></div>
+
+    <div><button type="button" class="gobrik-logo" onclick="closeKnack()" aria-label="Return to Home page"></button></div>
+    
+   
+    
+    <div class="button-list" style="display:flex;position:absolute;right:0;width:fit-content;margin-right:15px;flex-flow:row;margin-top:22px;"m>
+      
+        
+            <div id="top-menu-login-button"><button type="button" class="top-menu-login-button"  onclick="openKnack()" >
+                login
+            </button></div>
+
+            <div id="top-close-buttoner" style="display: none;"><button type="button" class="top-close-button" onclick="closeKnack()" aria-label="Click to open settings page"></button></div>
+
+
+            <div><button type="button" class="top-settings-button" onclick="openSettings()" aria-label="Click to open settings page"></button></div>
+
+           <!-- <div><button type="button" style="background: none;border: none;" class="settings-">
+                <img src="../svgs/language-button2.svg" height="30px">
+            </button></div>-->
+               
+        </div>
+    </div>
+</div> 
+
+
+
   <div class="knack-overlay-content">
     <!--<div id="loading-box">
       <div id="header" class="top-menu">
