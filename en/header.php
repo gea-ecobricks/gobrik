e837597f684b0f6d11930a574766ac5aa80371e1
Russell Maier <russ@earthen.io>
2023-03-08 00:35:35 +0800

    33

--- en/header.php
+++ en/header.php
@@ -47,16 +47,16 @@
 <link rel="stylesheet" type="text/css" href="../landing-stylesheet.css?v1.9">-->
 
 <!-- Main Landing stylesheet -->
-<link rel="stylesheet" type="text/css" href="../march-stylesheet.css?v8.8">
+<link rel="stylesheet" type="text/css" href="../march-stylesheet.css?v8.9">
 
 <!--LEGACY Sheets to consolidate-->
 
 <!-- Primary stylesheet -->
-<link rel="stylesheet" type="text/css" href="../main-stylesheet-march.css?v1.93">
+<link rel="stylesheet" type="text/css" href="../main-stylesheet-march.css?v1.94">
 
 
 <!-- Knack Stylesheet -->
-<link rel="stylesheet" type="text/css" href="../knack-stylesheet-march.css?v0.2">
+<link rel="stylesheet" type="text/css" href="../knack-stylesheet-march.css?v0.3">
 
 
 
@@ -83,14 +83,14 @@
 <script src="../accessibility.js" defer></script>
 
 <!--These are the scripts for the new Richard Landing page-->
-<script src="../richard-scripts.js?v=2"></script>
+<script src="../richard-scripts.js?v=3"></script>
 
 <!--This enables the Light and Dark mode switching-->
 <script type="module" src="https://unpkg.com/dark-mode-toggle"></script>
  
 <!--Stylesheets for light and dark mode.  They need to be called here-->
-<link rel="stylesheet" href="../light.css?v3.92" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
-<link rel="stylesheet" href="../dark.css?v3.92" media="(prefers-color-scheme: dark)">
+<link rel="stylesheet" href="../light.css?v3.93" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
+<link rel="stylesheet" href="../dark.css?v3.93" media="(prefers-color-scheme: dark)">
  
 
 
