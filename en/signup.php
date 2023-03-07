15dc69e434f7856bd4ba91f67e26b212e3df26ad
Russell Maier <russ@earthen.io>
2023-03-07 10:56:28 +0800

    signup

---
+++ en/signup.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php $lang='en';?>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+
+<!-- Meta tags for page display and search engine listing
+AND UNIQUE to HTML Pages--> 
+
+<title>GoBrik</title>
+<meta name="keywords" content="gobrik, ecobrick app, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration,  plastic solved, drop off, exchange, marketplace, plastic sequestration, aes plastic, plastic offsetting, ecological accounting, plastic accounting">
+<meta name="description" content="Manage your ecobricks, projects and plastic transition. By putting our plastic to good use, together we can build our greenest visions.">
+<meta name="author" content="Global Ecobrick Alliance">
+
+<!-- Facebook Open Graph Tags for social sharing-->
+
+<meta property="og:url"           content="https://www.gobrik.com/<?php echo $lang; ?>">
+<meta property="og:type"          content="app">
+<meta property="og:title"         content="GoBrik">
+<meta property="og:description"   content="Manage your ecobricks, projects and plastic transition. By putting our plastic to good use, together we can build our greenest visions." >
+<meta property="og:image"         content="https://www.gobrik.com/images/social-banner-1200px.png" >
+<meta property="fb:app_id"  content="1781710898523821" >
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="1000" />
+<meta property="og:image:alt"     content="A metaphorical road winding into the distance with various ecobrick and earth constructions along side it and the GoBrik logo floating above"/>
+<meta property="og:locale" content="en_GB, id_ID, es_ES" />
+
+<link rel="preload" as="image" href="https://gobrik.com/svgs/Happy-turtle-dolphin-opti2.svg">
+
+<?php require_once ("header.php");?>
+ 
+<style>
+
+</style>
+
+</head>
+
+<body id="full-page" class="accessibility-plugin-ac">
+
+<?php include 'top-menu.php';?>
+
+<?php require_once ("knack-curtain.php");?>
+
+</div> <!--landing page close-->
+
+<?php require_once ("settings-curtain.php");?>
+
+<?php require_once ("menu-curtain.php");?>
+         
+</body>
+
+</html>
