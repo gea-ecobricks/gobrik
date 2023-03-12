52befda7772ac71bd824530a67b58e8a2e116784
Russell Maier <russ@earthen.io>
2023-03-12 16:27:02 +0800

    upsy

--- en/settings-curtain.php
+++ en/settings-curtain.php
@@ -6,7 +6,31 @@
 
     <div class="overlay-content-settings">
         
-            <div class="compro-toggle" style="font-size:1.3em;padding 20px;">
+         
+                        
+        <div class="language-box">
+            <div><button type=button aria-label="Switch to English" class="language-selector" onclick="location.href='../en/'">EN</button></div>
+            <div><button type=button aria-label="Switch to Indonesian" class="language-selector" onclick="location.href='../id/'">ID</button></div>
+            <div><button type=button aria-label="Switch to French" class="language-selector" onclick="location.href='../fr/'">FR</button></div>
+        </div> 
+
+        <div class="menu-overlay-content">
+              
+              
+              <div class="menu-page-item" onclick="location.href='index.php'" aria-label="Go to top of page">Home</div>
+      
+              <div class="menu-page-item" onclick="location.href='signup.php'" aria-label="Go to top of page">Login  |  Sign up</div>
+      
+              <div class="menu-page-item" onclick="location.href='index.php'" aria-label="Go to top of page">Brikchain</div>
+      
+              <div class="menu-page-item" onclick="location.href='https:\/\|\:ecobricks.org/en/gobrik.php'" aria-label="About the GoBrik project">About GoBrik</div>
+              
+      
+                <p>GoBrik is developed by the Global Ecobrick Alliance a not-for-profit Earth enterprise focused on plastic transtion.</p>
+                
+              </div>
+
+              <div class="compro-toggle" style="font-size:1.3em;padding 12px;">
                 <dark-mode-toggle
                 id="dark-mode-toggle-1"
                 legend=""
@@ -16,20 +40,14 @@
                 remember="Remember this"
                 ></dark-mode-toggle>
             </div>
-                        
-        <div class="language-box">
-            <div><button type=button aria-label="Switch to English" class="language-selector" onclick="location.href='../en/'">EN</button></div>
-            <div><button type=button aria-label="Switch to Indonesian" class="language-selector" onclick="location.href='../id/'">ID</button></div>
-            <div><button type=button aria-label="Switch to French" class="language-selector" onclick="location.href='../fr/'">FR</button></div>
-        </div> 
 
      
-
-        <div class="text-adjust">
+ 
+    <!--    <div class="text-adjust">
             <button class="increase-plugin-ac" aria-label="Increase Text Size">+A</button>
             <button class="normal-plugin-ac" aria-label="Return Text Size to Default">A</button>
             <button class="decrease-plugin-ac" aria-label="Decrease Text Size">A-</button>
-        </div>
+        </div>-->
 
     </div>                                                              
 </div>
