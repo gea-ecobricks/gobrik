5d74451dc110bf76d4868bbdc01597dc72c1e5a7
Russell Maier <russ@earthen.io>
2023-03-15 20:27:15 +0800

    96

--- en/header.php
+++ en/header.php
@@ -47,7 +47,7 @@
 <link rel="stylesheet" type="text/css" href="../landing-stylesheet.css?v1.9">-->
 
 <!-- Main Landing stylesheet -->
-<link rel="stylesheet" type="text/css" href="../march-stylesheet.css?v96.5">
+<link rel="stylesheet" type="text/css" href="../march-stylesheet.css?v96.6">
 
 <!--LEGACY Sheets to consolidate-->
 
@@ -80,8 +80,8 @@ This enables accessibility text scaling on the page by the user-->
 <script type="module" src="https://unpkg.com/dark-mode-toggle"></script>
  
 <!--Stylesheets for light and dark mode.  They need to be called here-->
-<link rel="stylesheet" href="../light.css?v4" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
-<link rel="stylesheet" href="../dark.css?v4" media="(prefers-color-scheme: dark)">
+<link rel="stylesheet" href="../light.css?v4.1" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
+<link rel="stylesheet" href="../dark.css?v4.1" media="(prefers-color-scheme: dark)">
  
 
 
