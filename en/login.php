351e7de604c95c2fd547cdf2cd9885eef2a22797
Russell Maier <russ@earthen.io>
2023-03-17 11:50:55 +0800

    my briks

--- en/login.php
+++ en/login.php
@@ -1,35 +1,50 @@
 <!DOCTYPE html>
-<html>
-<head>
-<?php $lang='en';?>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
 
+<html lang="en">
+
+<head>
 <!-- Meta tags for page display and search engine listing
 AND UNIQUE to HTML Pages--> 
 
-<title>GoBrik</title>
+<title>GoBrik | βeta</title>
 <meta name="keywords" content="gobrik, ecobrick app, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration,  plastic solved, drop off, exchange, marketplace, plastic sequestration, aes plastic, plastic offsetting, ecological accounting, plastic accounting">
 <meta name="description" content="Manage your ecobricks, projects and plastic transition. By putting our plastic to good use, together we can build our greenest visions.">
 <meta name="author" content="Global Ecobrick Alliance">
 
 <!-- Facebook Open Graph Tags for social sharing-->
-
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
 <meta property="og:url"           content="https://www.gobrik.com/<?php echo $lang; ?>">
 <meta property="og:type"          content="app">
 <meta property="og:title"         content="GoBrik">
 <meta property="og:description"   content="Manage your ecobricks, projects and plastic transition. By putting our plastic to good use, together we can build our greenest visions." >
 <meta property="og:image"         content="https://www.gobrik.com/images/social-banner-1200px.png" >
 <meta property="fb:app_id"  content="1781710898523821" >
-<meta property="og:image:width" content="1200" />
-<meta property="og:image:height" content="1000" />
-<meta property="og:image:alt"     content="A metaphorical road winding into the distance with various ecobrick and earth constructions along side it and the GoBrik logo floating above"/>
-<meta property="og:locale" content="en_GB, id_ID, es_ES" />
+<meta property="og:image:width" content="1200" >
+<meta property="og:image:height" content="1000" >
+<meta property="og:image:alt"     content="A metaphorical road winding into the distance with various ecobrick and earth constructions along side it and the GoBrik logo floating above">
+<meta property="og:locale" content="en_GB">
 
-<link rel="preload" as="image" href="https://gobrik.com/svgs/Happy-turtle-dolphin-opti2.svg">
+<!--<link rel="preload" as="image" href="https://gobrik.com/svgs/casandra.svg">-->
+
+<script type="text/javascript">app_id="5b8c28c2a1152679c209ce0c";distribution_key="dist_53"; async</script>
+        
+<script type="text/javascript" src="https://loader.knack.com/5b8c28c2a1152679c209ce0c/dist_53/knack.js" async></script>
 
 <?php require_once ("header.php");?>
  
 <style>
+
+.kn-mobile-controls { 
+  z-index: 26 !important;
+  margin-top: 7px;
+}
+
+.main-menu-button {
+  display:none;
+}
+
+.kn-content {
+  margin-top: -70px;}
 
 </style>
 
@@ -37,15 +52,14 @@ AND UNIQUE to HTML Pages-->
 
 <body id="full-page" class="accessibility-plugin-ac">
 
-<?php include 'top-menu.php';?>
+    <?php include 'top-menu.php';?>
 
-<?php require_once ("knack-curtain.php");?>
-
-</div> <!--landing page close-->
+  <!-- Main knack loader--> 
+  <div id="knack-dist_53">
+        <div class="loader-spin"></div>
+  </div>
 
 <?php require_once ("settings-curtain.php");?>
-
-<?php require_once ("menu-curtain.php");?>
          
 </body>
 
