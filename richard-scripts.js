620f6fde588e42116182df85eb91638f2e11b145
Russell Maier <russ@earthen.io>
2023-03-19 14:03:18 +0800

    space

--- richard-scripts.js
+++ richard-scripts.js
@@ -31,8 +31,7 @@ function openGallery() {
   document.getElementById("right-settings-overlay").style.width = "90%";
   document.body.style.overflowY = "hidden";
   document.body.style.maxHeight = "101vh";
-  document.getElementById("regular-ecobricks").display ="none";
-
+  document.getElementById("gallery-overlay-button").display = "none";
 }
 
 
