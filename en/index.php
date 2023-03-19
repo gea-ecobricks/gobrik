1a54dbbd7e68bd6c49d85df71654a70708aad4f8
Russell Maier <russ@earthen.io>
2023-03-19 20:37:13 +0800

    movint it

--- en/index.php
+++ en/index.php
@@ -141,58 +141,6 @@ if (!$conn) {
 
 
 
-<div id="regular-ecobricks">     
-   <div class="gallery-background">
-  <!-- <div class="grey-gradient" style="background-image: linear-gradient(grey,grey, #30FFFF);height:60vh;margin-bottom:-60vh;max-height:fit-content;"></div>
-
--->
-
- <!--   echo '
-            <div class="brik-co2">'.$row["ecobrick_brk_amt"].' BRK<br>'.$row["weight_in_g"].'g<br>'.$row["CO2_kg"].' CO2e
-                    </div>
-                    </div>';-->
-            
-
-
-
-        <div class="gallery-content-block">
-            
-            <div class="flex-container">
-
-                <?php
-
-                $sql = "SELECT * FROM vw_gallery_feed ;";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                   array_reverse ($result);
-                // output data of each row
-                while( $row= $result->fetch_assoc()) {
-
-                echo '
-                <div class="gal-photo">
-                    <div class="photo-box">
-                        <a href="details-ecobrick-page.php?serial_no='.$row["ecobrick_unique_id"].'"><img src="'.$row["thumb_url"].'?v=2" loading="lazy" alt="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'" title="Ecobrick '.$row["ecobrick_unique_id"].' by '.$row["ecobrick_owner"].' in '.$row["location"].'"></a>
-                    </div>
-                </div>';
-        
-                }
-
-                } else {
-                echo "Failed to connect to the Brikchain database";
-                }
-
-                ?>
-
-            </div>
-           
-
-        </div><!--closes gallery content block-->
-
-        <div class="gallery-header">
-<div class="gallery-live-text"><span class="blink">⬤ </span> Latest authenticated ecobricks</div>
-</div>
-    </div><!--closes gallery background-->
-</div><!--closes regular ecobrick curtain-->
      
 <!--
  <div class="bio-top"><img src="../svgs/biosphere-top-day.webp" style="width:100%" alt="Together we can contribute to the biosphere"></div>-->
