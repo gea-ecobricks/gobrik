b90af071bca6956ae15326ab87c2148971882b03
Russell Maier <russ@earthen.io>
2023-03-26 23:05:56 +0800

    robbie

--- en/go.php
+++ en/go.php
@@ -51,7 +51,7 @@ AND UNIQUE to HTML Pages-->
  <!--Stylesheets for light and dark mode.  They need to be called here-->
 <link rel="stylesheet" href="../light.css?v5.7" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
 
-<!--<link rel="stylesheet" href="../dark.css?v5.7" media="(prefers-color-scheme: dark)">-->
+<link rel="stylesheet" href="../dark.css?v=7" media="(prefers-color-scheme: dark)">
  
 <style>
 
