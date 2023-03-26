228d0fbecd1026b2a87fbee6de41beafd0624de9
Russell Maier <russ@earthen.io>
2023-03-26 16:25:45 +0800

    try

--- en/index.php
+++ en/index.php
@@ -1086,7 +1086,7 @@ function closeGallery() {
 
         <div id="regularecobricks">  
 
-          <button  type="button" id="gal-button" style="down-arrow" onclick="openGallery()" aria-label="open gallery" style="background-color:#ff00001c;right:0;z-index:20;border:none;width:100%;height:100%;with:100%;position:absolute;cursor: pointer;"><div class="down-arrow"></div></button>
+          <button  type="button" id="gal-button" style="down-arrow" onclick="openGallery()" aria-label="open gallery" style="background-color:#ff00001c;right:0;z-index:20;border:none;width:100%;height:100%;with:100%;position:absolute;cursor: pointer;"></button>
 
 
           <div class="gallery-header" style="display:flex;flex-flow:column;">
