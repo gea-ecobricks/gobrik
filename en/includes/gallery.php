e04f7ebec78fae341e8cfa1861116e2546f1ba35
Russell Maier <russ@earthen.io>
2023-05-02 16:55:57 +0800

    desc

--- en/includes/gallery.php
+++ en/includes/gallery.php
@@ -20,7 +20,7 @@
 
                 <?php
 
-$sql = "SELECT * FROM vw_gallery_feed ORDER BY id DESC;";
+$sql = "SELECT * FROM vw_gallery_feed DESC;";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 // output data of each row
