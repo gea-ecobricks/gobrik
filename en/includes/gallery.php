93058b7b75e0d57da0a3268013e9b8067b146798
Russell Maier <russ@earthen.io>
2023-05-02 20:40:14 +0800

    alan fix

--- en/includes/gallery.php
+++ en/includes/gallery.php
@@ -25,9 +25,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 // output data of each row
 while($row = $result->fetch_assoc()) {
-//$row = $result->fetch_assoc();
-
-        
+$row = $result->fetch_assoc();
 
                 echo '
                 <div class="gal-photo">
