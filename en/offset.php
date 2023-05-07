d9f43c05aae0a6f978f8a904e71efa382b2081ab
Russell Maier <russ@earthen.io>
2023-05-07 11:26:41 +0800

    maybe

--- en/offset.php
+++ en/offset.php
@@ -14,16 +14,35 @@ AND UNIQUE to HTML Pages-->
 
 <!-- Facebook Open Graph Tags for social sharing-->
 
-<meta property="og:url"           content="https://www.gobrik.com/en/courses.php">
+
+<title>Offset Your Plastic & CO2 | GoBrik.com</title>
+<meta name="keywords" content="plastic offsetting, carbon offsetting, plastic offset, carbon offset, ecobricks, ecobrick, plastic solved, eco-brick, gobrik, bottle brick, plastic solution, brikcoin, gobrik, ">
+<meta name="description" content="Take responsibility for your plastic consumption by offseting on the brikcoin blockchain to support the global ecobrick community">
+
+
+<!-- Facebook Open Graph Tags for social sharing-->
+
+<meta property="og:url"           content="https://www.gobrik.com/en/offset.php">
 <meta property="og:type"          content="website">
-<meta property="og:title"         content="Courses | GoBrik.com">
-<meta property="og:description"   content="Regenerative courses in Plastic Transition to accelerate your green living." >
-<meta property="og:image"         content="https://www.gobrik.com/images/courses-banner-750px.png" >
+<meta property="og:title"         content="Offset Your Plastic & CO2">
+<meta property="og:description"   content="Take responsibility for your plastic consumption by offseting on the brikcoin blockchain to support the global ecobrick community" >
+<meta property="og:image"         content="https://www.gobrik.com/images/offset-banner.png" >
 <meta property="fb:app_id"  content="1781710898523821" >
-<meta property="og:image:width" content="750" />
+<meta property="og:image:width" content="1000" />
 <meta property="og:image:height" content="" />
-<meta property="og:image:alt"     content="Ecobrick and Earthen Courses"/>
-<meta property="og:locale" content="en_GB, es_ES" />
+<meta property="og:image:alt"     content="A balance of grey and green impacts"/>
+<meta property="og:locale" content="en_GB, id_ID, es_ES" />
+
+<meta property="article:modified_time" content="2023-05-07T09:14:13+00:00" />
+
+	<meta name="author" content="GoBrik.com">
+  
+  <meta property="og:type" content="page" />
+ <meta property="og:site_name" content="GoBrik.com" />
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
+ <meta property="article:modified_time" content="2022-08-14T09:14:13+00:00" />
+ <meta property="og:image:type" content="image/png" />
+ <meta name="author" content="GoBrik.com" />
 
 
 
