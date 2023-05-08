8c14bec07cdc51d36fec19e6a193a4b174880552
Russell Maier <russ@earthen.io>
2023-05-08 17:19:55 +0800

    up shop

---
+++ en/shop.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+<!-- Meta tags for page display and search engine listing
+AND UNIQUE to HTML Pages--> 
+
+
+
+<title>Regen Store | Shop GoBrik for Green products from regenerative makers.</title>
+<meta name="keywords" content="green products, regenerative products, earth products, earthwands, earthcups, brikcoin, gobrik, regen store, green store">
+<meta name="description" content="Shop green products directly from regenerative makers.">
+
+
+<!-- Facebook Open Graph Tags for social sharing-->
+
+<meta property="og:url"           content="https://www.gobrik.com/en/shop.php">
+<meta property="og:type"          content="website">
+<meta property="og:title"         content="Regen Store | GoBrik.com">
+<meta property="og:description"   content="Shop green products directly from regenerative makers." >
+<meta property="og:image"         content="https://www.gobrik.com/images/regen-store-banner-flat-1000px.png" >
+<meta property="fb:app_id"  content="1781710898523821" >
+<meta property="og:image:width" content="1000" />
+<meta property="og:image:height" content="" />
+<meta property="og:image:alt"     content="Regenerartive green products"/>
+<meta property="og:locale" content="en_GB, id_ID, es_ES" />
+
+<meta property="article:modified_time" content="2023-05-07T09:14:13+00:00" />
+
+	<meta name="author" content="GoBrik.com">
+  
+  <meta property="og:type" content="page" />
+ <meta property="og:site_name" content="GoBrik.com" />
+ <meta property="article:publisher" content="https://web.facebook.com/ecobricks.org" />
+ <meta property="og:image:type" content="image/png" />
+ <meta name="author" content="GoBrik.com" />
+
+
+
+
+<script type="text/javascript">app_id="5b8c28c2a1152679c209ce0c";distribution_key="dist_45"; async</script>
+        
+<script type="text/javascript" src="https://loader.knack.com/5b8c28c2a1152679c209ce0c/dist_45/knack.js" async></script>
+
+<?php require_once ("includes/header.php");?>
+
+
+
+<!--Default Light Styles to load first-->
+<link rel="preload" href="../light.css?v5.8" as="style" onload="this.rel='stylesheet'">
+
+<!--LEGACY Sheets to consolidate-->
+<link rel="stylesheet" type="text/css" href="../april-old-knack-to-transition.css?v=1.3">
+
+<!-- Old stylesheet 
+<link rel="stylesheet" type="text/css" href="../main-stylesheet-march.css?v1.1">
+<link rel="stylesheet" type="text/css" href="../knack-stylesheet-march.css?v0.5">
+-->
+
+<!-- Main Landing stylesheet -->
+<link rel="stylesheet" type="text/css" href="../2023-stylesheet.css?v1.1">
+
+ <!--Stylesheets for light and dark mode.  They need to be called here-->
+<link rel="stylesheet" href="../light.css?v5.8" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)">
+
+<link rel="stylesheet" href="../dark.css?v=7.2" media="(prefers-color-scheme: dark)">
+ 
+<style>
+
+#kn-app-header {
+  background-color: var(--general-background) !important;
+}
+
+#header {
+  box-shadow: none !important;
+}
+
+
+.kn-mobile-controls { 
+  z-index: 26 !important;
+}
+
+.main-menu-button {
+  display:none;
+}
+
+.kn-content {
+  margin-top: -40px;}
+
+  
+
+.loader-spin { 
+  position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    margin:auto;
+    z-index: 5;
+    background: url('../svgs/bottle-loader-day-7.svg') center no-repeat;
+      box-sizing: border-box;
+   background-size: 100px;
+
+ }
+
+</style>
+
+</head>
+
+<body id="full-page" class="accessibility-plugin-ac">
+
+    <?php include 'includes/top-menu.php';?>
+
+  <!-- Main knack loader--> 
+  <div id="knack-dist_45">
+        <div class="loader-spin"></div>
+  </div>
+
+<?php require_once ("includes/settings-curtain.php");?>
+         
+</body>
+
+</html>
---
+++ shop.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php $dist='43';?>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
+
+
+<!-- Meta tags for page display and search engine listing
+AND UNIQUE to HTML Pages-->
+
+<title>Courses | GoBrik.com</title>
+<meta name="keywords" content="ecobrick courses, starter workshop, trainer course, trainer of trainer ecobrick course, gobrik, ecobrick app, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration,  plastic solved, drop off, exchange, marketplace">
+<meta name="description" content="Regenerative courses in Plastic Transition to accelerate your green living.">
+
+
+<!-- Facebook Open Graph Tags for social sharing-->
+
+<meta property="og:url"           content="https://www.gobrik.com/courses.php">
+<meta property="og:type"          content="website">
+<meta property="og:title"         content="Courses | GoBrik.com">
+<meta property="og:description"   content="Regenerative courses in Plastic Transition to accelerate your green living." >
+<meta property="og:image"         content="https://www.gobrik.com/images/courses-banner-750px.png" >
+<meta property="fb:app_id"  content="1781710898523821" >
+<meta property="og:image:width" content="750" />
+<meta property="og:image:height" content="" />
+<meta property="og:image:alt"     content="Plastic and the biosphere course"/>
+<meta property="og:locale" content="en_GB, es_ES" />
+
+
+<!-- sets page meta tags:  be sure to add page name-->
+<?php require_once ("header.php");?>
+
+<!-- sets header and core of page:  be sure to add page name-->
+<?php require_once ("core.php");?>
+
+<!-- sets footer of page:  be sure to add page name-->
+<?php require_once ("footer.php");?>
+
+	 
+</body>
+</html>
