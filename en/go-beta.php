b0bb2a7a2364c2c64e8243d91cbbe1788704f88c
Russell Maier <russ@earthen.io>
2023-11-02 17:16:53 +0000

    char

--- en/go-beta.php
+++ en/go-beta.php
@@ -8,6 +8,7 @@ AND UNIQUE to HTML Pages-->
 
 <title>GoBrik | BETA </title>
 
+<meta charset="UTF-8"> 
 
 <meta name="keywords" content="gobrik, ecobrick app, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration,  plastic solved, drop off, exchange, marketplace, plastic sequestration, aes plastic, plastic offsetting, ecological accounting, plastic accounting">
 <meta name="description" content="Manage your ecobricks, projects and plastic transition. By putting our plastic to good use, together we can build our greenest visions.">
