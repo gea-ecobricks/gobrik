2db806331ca110e1116c401983fca4bddaa9ea51
Russell Maier <russ@earthen.io>
2023-11-02 18:13:29 +0000

    go betaff

--- en/go-beta.php
+++ en/go-beta.php
@@ -30,9 +30,51 @@ AND UNIQUE to HTML Pages-->
         
 <script src="https://loader.knack.com/654387b48cf6b600295bba26/dist_2/knack.js"></script>
 
-
-
 <script async src="https://arc.io/widget.min.js#mJFtQsHF"></script>
+
+
+<!-- This calls our webmanifest wich turns our page into a progressive web app (PWA) -->
+<link rel="manifest" href="manifest.webmanifest">
+
+	
+<!-- This allows GoBrik to be used a PWA on iPhones -->
+
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="width=device-width, initial-scale=1.8, target-densitydpi=160, maximum-scale=1, user-scalable = no">
+<meta name="apple-mobile-web-app-title" content="GoBrik">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	
+<link rel="apple-touch-icon" sizes="57x57" href="icons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="icons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="icons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="icons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="icons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="icons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="icons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="icons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="icons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
+	
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="icons/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+	
+<!-- Launch screens for iOS - Not sure if this working -->
+
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<link href="icons/splashes/apple_splash_2048.png" sizes="2048x2732" rel="apple-touch-startup-image" />
+<link href="icons/apple_splash_1668.png" sizes="1668x2224" rel="apple-touch-startup-image" />
+<!--<link href="/apple_splash_1536.png" sizes="1536x2048" rel="apple-touch-startup-image" />
+<link href="/apple_splash_1125.png" sizes="1125x2436" rel="apple-touch-startup-image" />
+<link shref="/apple_splash_1242.png" sizes="1242x2208" rel="apple-touch-startup-image" />
+<link href="/apple_splash_750.png" sizes="750x1334" rel="apple-touch-startup-image" />
+<link href="/apple_splash_640.png" sizes="640x1136" rel="apple-touch-startup-image" />-->
+
+
+
 
 
 
@@ -69,7 +111,7 @@ AND UNIQUE to HTML Pages-->
 
  @font-face {
   font-family: "Arvo";
-  src: url("../fonts/Arvo-Regular.");
+  src: url("../fonts/Arvo-Regular.fff");
   font-display: swap;
  }
 
