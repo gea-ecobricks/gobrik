ecd0561c175221d0be2e62a8d727f01710af7dcd
Russell Maier <russ@earthen.io>
2023-11-02 17:34:39 +0000

    2.14

--- en/go.php
+++ en/go.php
@@ -10,7 +10,7 @@
 <!-- Meta tags for page display and search engine listing
 AND UNIQUE to HTML Pages--> 
 
-<title>GoBrik | 2.13</title>
+<title>GoBrik | 2.14</title>
 
 
 <meta name="keywords" content="gobrik, ecobrick app, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration,  plastic solved, drop off, exchange, marketplace, plastic sequestration, aes plastic, plastic offsetting, ecological accounting, plastic accounting">
