7a4cc97e6d8a50fabb0022a1cd5fa83f08856e59
Russell Maier <russ@earthen.io>
2023-11-03 16:22:47 +0000

    encoding

--- en/go.php
+++ en/go.php
@@ -6,7 +6,7 @@
 <!-- Meta tags for page display and search engine listing
 AND UNIQUE to HTML Pages--> 
 
-<title>GoBrik | BETA 2.4 </title>
+<title>GoBrik | BETA 2.5 </title>
 
 <meta name="keywords" content="gobrik, ecobrick app, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration,  plastic solved, drop off, exchange, marketplace, plastic sequestration, aes plastic, plastic offsetting, ecological accounting, plastic accounting">
 <meta name="description" content="Manage your ecobricks, projects and plastic transition. By putting our plastic to good use, together we can build our greenest visions.">
@@ -25,9 +25,6 @@ AND UNIQUE to HTML Pages-->
 <meta property="og:locale" content="en_GB">
 
 
-<script>app_id="5b8c28c2a1152679c209ce0c";distribution_key="dist_53"; </script>
-        
-<script src="https://loader.knack.com/5b8c28c2a1152679c209ce0c/dist_53/knack.js"></script>
 
 <script async src="https://arc.io/widget.min.js#mJFtQsHF"></script>
 
@@ -173,6 +170,9 @@ AND UNIQUE to HTML Pages-->
 
 <link rel="stylesheet" type="text/css" href="../2023-stylesheet.css?v1.5">
 
+<script>app_id="5b8c28c2a1152679c209ce0c";distribution_key="dist_53"; </script>
+        
+<script src="https://loader.knack.com/5b8c28c2a1152679c209ce0c/dist_53/knack.js"></script>
 
 </head>
 
