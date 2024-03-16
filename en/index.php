15e00320f08c6452c8dd0b6259909f29edfc30c1
Russell Maier <russ@earthen.io>
2024-03-16 11:10:12 +0800

    no main

--- en/index.php
+++ en/index.php
@@ -6,7 +6,7 @@
 <!-- Meta tags for page display and search engine listing
 AND UNIQUE to HTML Pages--> 
 
-<title>GoBrik</title>
+<title>GoBrik Landing</title>
 <meta name="keywords" content="gobrik, ecobrick app, eco, brick, eco brick, ecobrick, eco-brick, eco, bricks, eco brick, ecobricks, eco-bricks, brik, briks, plastic, plastic management, carbon sequestration,  plastic solved, drop off, exchange, marketplace, plastic sequestration, aes plastic, plastic offsetting, ecological accounting, plastic accounting">
 <meta name="description" content="Manage your ecobricks, projects and plastic transition. By putting our plastic to good use, together we can build our greenest visions.">
 <meta name="author" content="Global Ecobrick Alliance">
